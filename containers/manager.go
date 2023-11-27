e541bdf20e3af01061e87d417a23f7e30627ce90
Dagem Tadesse <dagem.seyfu@gmail.com>
2023-11-27 20:21:00 +0300

    refactor: docker container management

---
+++ containers/manager.go
@@ -0,0 +1,130 @@
+package containers
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/go-connections/nat"
+	"io"
+	"os"
+)
+
+type ErrorLine struct {
+	Error       string      `json:"error"`
+	ErrorDetail ErrorDetail `json:"errorDetail"`
+}
+
+type ErrorDetail struct {
+	Message string `json:"message"`
+}
+
+type ContainerManager struct {
+	cli *client.Client
+}
+
+func NewManager() (*ContainerManager, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	return &ContainerManager{cli}, err
+}
+
+func (cm *ContainerManager) BuildImage(ctx context.Context, contextPath string, projectName string) error {
+	err := os.Chdir(contextPath)
+	if err != nil {
+		return err
+	}
+
+	buildOptions := types.ImageBuildOptions{
+		Tags:           []string{projectName},
+		SuppressOutput: false,
+		Remove:         true,
+		ForceRemove:    true,
+		PullParent:     true,
+		Dockerfile:     "dockerfile",
+	}
+
+	buildContext, err := archive.TarWithOptions(".", &archive.TarOptions{})
+	if err != nil {
+		return err
+	}
+
+	buildResponse, err := cm.cli.ImageBuild(ctx, buildContext, buildOptions)
+	if err != nil {
+		return err
+	}
+
+	defer buildResponse.Body.Close()
+
+	// Print build output
+	return checkError(buildResponse.Body)
+}
+
+func (cm *ContainerManager) CreateContainer(ctx context.Context, imageName string) (string, error) {
+	hostConfig := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"3000/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: "3000",
+				},
+			},
+		},
+	}
+
+	containerConfig := &container.Config{
+		Image: imageName,
+		ExposedPorts: nat.PortSet{
+			"3000/tcp": struct{}{},
+		},
+	}
+
+	resp, err := cm.cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, "")
+
+	return resp.ID, err
+}
+
+func (cm *ContainerManager) StartContainer(ctx context.Context, containerId string) error {
+	err := cm.cli.ContainerStart(ctx, containerId, types.ContainerStartOptions{})
+	return err
+}
+
+func (cm *ContainerManager) StopContainer(ctx context.Context, containerId string) error {
+	err := cm.cli.ContainerStop(ctx, containerId, container.StopOptions{})
+	return err
+}
+
+func (cm *ContainerManager) DeleteContainer(ctx context.Context, containerId string) error {
+	err := cm.cli.ContainerRemove(ctx, containerId, types.ContainerRemoveOptions{})
+	return err
+}
+
+func checkError(reader io.Reader) error {
+	var lastLine string
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+		fmt.Println(scanner.Text())
+	}
+
+	errLine := &ErrorLine{}
+	err := json.Unmarshal([]byte(lastLine), errLine)
+	if err != nil {
+		return errors.New("unable to parse build output")
+	}
+
+	if errLine.Error != "" {
+		return errors.New(errLine.Error)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -4,89 +4,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
-	"os"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/archive"
-	"github.com/docker/go-connections/nat"
+	"pulsar/poc/containers"
 )
-
-func buildDockerImage(ctx context.Context, cli *client.Client, imageName, contextPath string) error {
-	err := os.Chdir(contextPath)
-	if err != nil {
-		panic(err)
-	}
-
-	buildOptions := types.ImageBuildOptions{
-		Tags:           []string{imageName},
-		SuppressOutput: false,
-		Remove:         true,
-		ForceRemove:    true,
-		PullParent:     true,
-		Dockerfile:     "dockerfile",
-	}
-
-	buildContext, err := archive.TarWithOptions(".", &archive.TarOptions{})
-	if err != nil {
-		return err
-	}
-
-	buildResponse, err := cli.ImageBuild(ctx, buildContext, buildOptions)
-	if err != nil {
-		return err
-	}
-	defer buildResponse.Body.Close()
-
-	// Print build output
-	_, err = io.Copy(os.Stdout, buildResponse.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func createAndStartContainer(ctx context.Context, cli *client.Client, imageName string) (string, error) {
-
-	hostConfig := &container.HostConfig{
-		PortBindings: nat.PortMap{
-			"3000/tcp": []nat.PortBinding{
-				{
-					HostIP:   "0.0.0.0",
-					HostPort: "3000",
-				},
-			},
-		},
-	}
-
-	containerConfig := &container.Config{
-		Image: imageName,
-		ExposedPorts: nat.PortSet{
-			"3000/tcp": struct{}{},
-		},
-	}
-
-	resp, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, "")
-
-	if err != nil {
-		return "", err
-	}
-
-	err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
-	if err != nil {
-		return "", err
-	}
-
-	return resp.ID, nil
-}
 
 func main() {
 	ctx := context.Background()
 
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	manager, err := containers.NewManager()
 	if err != nil {
 		panic(err)
 	}
@@ -94,13 +18,35 @@ func main() {
 	imageName := "your-serverless-app:latest"
 	contextPath := "./app" // path to your GoLang application
 
-	err = buildDockerImage(ctx, cli, imageName, contextPath)
+	fmt.Println("Creating Docker image for the project")
+	err = manager.BuildImage(ctx, contextPath, imageName)
 	if err != nil {
 		fmt.Printf("Failed to build Docker image: %v\n", err)
 		return
 	}
 
-	createAndStartContainer(ctx, cli, imageName)
+	fmt.Println("Creating Docker Container for the project")
+	containerId, err := manager.CreateContainer(ctx, imageName)
+	if err != nil {
+		fmt.Printf("Failed to create Docker container: %v\n", err)
+		return
+	}
 
-	fmt.Printf("Docker image '%s' built successfully!\n", imageName)
+	fmt.Println("Starting starting the project container")
+	err = manager.StartContainer(ctx, containerId)
+	if err != nil {
+		fmt.Printf("Failed to start Docker container: %v\n", err)
+	}
+
+	fmt.Println("Stopping the project container")
+	err = manager.StopContainer(ctx, containerId)
+	if err != nil {
+		fmt.Printf("Failed to start Docker container: %v\n", err)
+	}
+
+	fmt.Println("Deleting the project container")
+	err = manager.DeleteContainer(ctx, containerId)
+	if err != nil {
+		fmt.Printf("Failed to start Docker container: %v\n", err)
+	}
 }
